bcd7a0d3222dc5e81a3c6902f22a5647711d565c
SeaWithHill <a13259768671@163.com>
2019-05-08 11:27:27 +0800

    初始导入

---
+++ src/test/java/redis/clients/jedis/Test.java
@@ -0,0 +1,24 @@
+package redis.clients.jedis;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+public class Test {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+
+}
